6e25f3cffbd3071ffaf73139414dd4af7bd4d1be
Sriraam-Kannan <131025595+Sriraam-Kannan@users.noreply.github.com>
2024-02-24 00:21:16 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>My React app</h1>
+      <h2>Using Vite + React.</h2>
+    </>
+  )
+}
+
+export default App
